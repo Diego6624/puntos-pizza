66b6fe9899173e6a13bd7f11fb0d311355b88369
JuanGarcia <juan2525.2004@gmail.com>
2025-10-17 22:24:00 -0500

    modif App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import Header from './components/header'
+import Footer from './components/footer'
 
 function App() {
 
   return (
     <>
-      <main>
+      <Header />
+      <main className='p-1.5'>
         <Outlet />
       </main>
+      <Footer />
     </>
   )
 }
