0141d4a66fdf5e0739049afc2a071f602ae50eaa
JuanGarcia <juan2525.2004@gmail.com>
2025-10-19 00:05:30 -0500

    Mejora header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
                     <img src={logoHeader} alt="Logo" className='h-auto w-min' />
                 </Link>
                 <div className='flex w-min h-auto border-2 border-cooper rounded-xl p-2 gap-3 items-center ml-4'>
-                    <input type="text" placeholder='Buscar productos...' className='h-max focus:outline-none' />
+                    <input type="text" placeholder='Buscar productos...' className='h-max w-50 focus:outline-none' />
                     <button className=''>
                         <Search size={20} className='text-cooper' />
                     </button>
@@ -19,28 +19,28 @@ const Header = () => {
             </div>
 
             {/* Navegación */}
-            <div className='w-full flex justify-evenly text-burnUmber text-lg'>
-                <Link to="/" className='font-semibold no-underline hover:text-cooper flex items-center gap-2'>
+            <div className='w-full flex justify-end text-burnUmber text-lg gap-15 pr-5'>
+                <Link to="/" className='font-semibold no-underline hover:text-cooper flex items-center gap-2 py-3'>
                     <Pizza size={20} />
                     Productos
                 </Link>
-                <Link to="/" className='font-semibold no-underline hover:text-cooper flex items-center gap-2'>
+                <Link to="/" className='font-semibold no-underline hover:text-cooper flex items-center gap-2 py-3'>
                     <UsersRound size={20} />
                     Nosotros
                 </Link>
-                <Link to="/" className='font-semibold no-underline hover:text-cooper flex items-center gap-2'>
+                <Link to="/" className='font-semibold no-underline hover:text-cooper flex items-center gap-2 py-3'>
                     <Phone size={20} />
                     Contacto
                 </Link>
             </div>
 
             {/* Botones de usuario */}
-            <div className='flex w-min h-auto justify-end items-center gap-4'>
-                <button className='bg-cooper h-full text-white p-2 rounded hover:bg-sunset flex items-center gap-2 whitespace-nowrap'>
+            <div className='flex w-min h-auto justify-end items-center gap-4 text-lg'>
+                <button className='bg-burnUmber h-full text-white hover:text-cornsilk p-2 rounded hover:bg-red-950 flex items-center gap-2 whitespace-nowrap'>
                     <User size={20} />
                     Iniciar Sesión
                 </button>
-                <button className="bg-cooper text-white p-2 rounded hover:bg-sunset flex items-center justify-center w-[40px] h-[40px]">
+                <button className="bg-burnUmber text-white hover:text-cornsilk p-2 rounded hover:bg-red-950 flex items-center justify-center w-[44px] h-[44px]">
                     <ShoppingCart size={20} />
                 </button>
             </div>
